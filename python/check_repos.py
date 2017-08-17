98d474e59e35f439a67d7b5f77df7d6056ae106a
mythos <kuangchengping@outlook.com>
2017-08-17 09:35:35 +0800

    脚本

--- python/check_repos.py
+++ python/check_repos.py
@@ -1,35 +1,29 @@
-import os
 import sys
 import subprocess
 
 
 # 执行命令,如果仓库没有变动就不输出
-def command(cmd):
+def command(cmd, content_line, count):
     result=[]
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         line = line.decode()
         if(line != "无文件要提交，干净的工作区\n"):
             result.append(line.rstrip())
-            #print(">"+line+"<")
         else:
-            result = []
-            return 0 # 如果没有变动就直接退出，不输出了   
-    # 输出所有结果              
+            return 0 # 如果git仓库没有变动就直接退出，不输出了   
+    # 输出所有结果 以及仓库信息
+    hang(count, content_line)
     for re in result:
-        print("> ",re)
+        print("",re)
 
 def hang(count,name):
-    print("."*40,count,"\n",name)
+    print("-"*50,count,"\n"+'-'*6,name)
 
-def title(title):
-    print(" "*20+title)
-
-def print_info(path,count):
-    temp = path.split("#")[0]
-    name = path.split("#")[1]
-    hang(count,path)
-    command('cd '+temp+' && git status')
+def print_info(line,count):
+    path = line.split("#")[0]
+    name = line.split("#")[1]
+    command('cd '+path+' && git status', line, count)
 
 def append_line(path):
     repos_path = input("请输入项目地址 : ")
@@ -41,7 +35,8 @@ def main():
     #if len(sys.argv) <= 1:
         #return 0
         
-    path = 'config/repos.md'
+    # 如果使用相对路径，是相对于当前终端的路径的
+    path = '/home/kcp/Application/Script/python/config/repos.md'
     for param in sys.argv:
         if param == '-h':
             print("帮助")
@@ -57,6 +52,7 @@ def main():
     count = 0
     for path in paths:    
         count = count+1
-        print_info(path,count)
+        if path.startswith('/'):
+            print_info(path,count)
     
 main()
--- python/config/repos.md
+++ python/config/repos.md
@@ -2,8 +2,14 @@
 /home/kcp/Documents/Notes/GhostPushLight #鬼吹灯项目
 /home/kcp/Documents/Notes/diary #个人日记
 /home/kcp/Documents/Notes/Kuangcp.github.io #个人博客
-/home/kcp/IdeaProjects/TestRun/MythRedis #客户端
+
+# /home/kcp/IdeaProjects/TestRun/MythRedis #客户端
 /home/kcp/PycharmProjects/PythonMythLearn #python学习记录
+/home/kcp/Application/Script # 脚本库
+
 /home/kcp/IdeaProjects/BaseLearn/JavaBase #Java基础学习
 /home/kcp/IdeaProjects/BaseLearn/JavaToolKit #Java工具包
-/home/kcp/IdeaProjects/test/RedisLearning #Redis学习记录之Java
+
+/home/kcp/Pictures/ImageRepo # 图片库
+
+# /home/kcp/IdeaProjects/test/RedisLearning #Redis学习记录之Java
