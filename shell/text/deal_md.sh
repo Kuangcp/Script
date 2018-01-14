d5543e30a87345785cc9f785e4298d4537352483
mythos <kuang.myth@foxmail.com>
2018-01-15 00:51:43 +0800

    递归

--- shell/text/deal_md.sh
+++ shell/text/deal_md.sh
@@ -1,7 +1,4 @@
 
-# TODO 自动更新指定目录的所有md文件的目录
-# ignore_flag=1
-
 read_dir(){
     for file in `ls $1`
     do
@@ -34,6 +31,7 @@ case $1 in
         printf "  $start%-20s$end%-20s\n" "-i|i|index" "更新索引目录"
         printf "  $start%-20s$end%-20s\n" "-c|c|current" "更新当前目录所有md文件的索引目录"
         printf "  $start%-20s$end%-20s\n" "-a|a|all" "更新指定目录下所有md文件的索引目录"
+        printf "  $start%-20s$end%-20s\n" "-al|al|alter" "更新指定git仓库下修改过的文件目录"
         printf "  $start%-20s$end%-20s\n" "no param" "更新索引目录";;
     -i | i | index)
         echo $2
@@ -51,50 +49,15 @@ case $1 in
         done;;
     -a | a | all)
         note_path='/home/kcp/Documents/Notes/Notes'
+        # note_path='/home/kcp/test/md'
         echo "开始更新全部, 目录: "$note_path
-
-        # ignore_list=('README.md','CODE_OF_CONDUCT.md')
-        read_dir $note_path
-
-        ;;
+        read_dir $note_path;;
+    -al | al | alter)
+        # TODO 
+        echo "更新修改文件的目录";;
     *)
         echo $1
         # 过滤掉 - 的字符串 
         result=`python3 ~/Application/Script/python/append_contents.py -a n $1`
         echo -e "$result";;
 esac
-
-
-
-
-# 始终有个bug，读取每一行时，行首的空格丢了！！
-# addToc(){
-#     echo -e "$2" 
-#     blank=`expr match "$line" "^ "`
-#     # echo $blank
-#     start_char=`expr match "$line" "^#*"` # 只能匹配到#并且后面有空格的字符串，返回出现次数
-#     if [ $start_char = 1 ]; then
-#         # echo -e "$2" 
-#         # echo ">>>>"$start_char
-#         titleWeight=`echo $2 | awk -F'#' '{print NF-1}'`
-#         # echo $titleWeight
-#     fi 
-#     echo ">>>>"$start_char
-# }
-
-# case $1 in 
-#     -h | h | help)
-#         echo "bash deal_md.sh <params>"
-#         echo "  -h|h|help    输出帮助信息"
-#         echo "  -i|i|index    生成索引目录"
-#         # echo ""
-#     ;;
-#     -i | i | index)
-#         echo -e "添加目录索引\n"
-#         while read -r "line"
-#         do
-#             # echo  "$line"
-#             addToc $2 "$line"
-#         done < $2
-#     ;;
-# esac
