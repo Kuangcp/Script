5a11bb40cfd6f245348b4f8fd5a1998bd9ee9139
mythos <kuangcp@aliyun.com>
2018-04-06 12:42:54 +0800

    完成通配符

--- shell/base/trash.sh
+++ shell/base/trash.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # 回收站
-# TODO怎么处理时间的更迭, 单进程已经做到了
 userDir=(`cd && pwd`)
 currentPath=`pwd`
 trashPath=$userDir'/.trash'
 # 日志
 logFile=$userDir'/.all.trash.log'
-# 存活时间
+# 存活时间 s
 timeOut=10
 # 轮询周期
 checkTime='5s'
@@ -18,13 +17,49 @@ init(){
         mkdir -phttp://www.cnblogs.com/276815076/archive/2011/10/30/2229286.html $trashPath
         touch $logFile
     fi
+    printf "Path : \033[0;32m"$trashPath"\n\033[0m"
+}
+# 延迟删除, 并隐藏屏蔽了信号, 不阻塞当前终端
+lazyDelete(){
+    result=`ps ux | grep trash.sh | wc -l`
+    # echo "$result"
+    # 限制只开一个进程, 为啥是3
+    if [ "$result" != "3" ];then
+        exit
+    else
+        fileNum=`ls $trashPath | wc -l`
+        while [ ! $fileNum = 0 ]; do
+            sleep $checkTime
+            log "→ check trash ..."
+            ls $trashPath | cat | while read line; do
+                currentTime=`date +%s`
+                removeTime=${line##*\.}
+                ((result=$currentTime-$removeTime))
+                # echo "$line | $result"
+                if [ $result -ge $timeOut ];then
+                    log "■ rm -rf $trashPath/$line"
+                    rm -rf "$trashPath/$line"
+                fi
+            done
+            fileNum=`ls $trashPath | wc -l`
+        done
+        log "▶ trash is empty. exit..."
+    fi
 }
 moveFile(){
-    printf "Path : \033[0;32m"$trashPath"\n\033[0m"
     fileName="$1";
+    # echo "input file : $fileName"
     deleteTime=`date +%s`
+    log "◆ prepare to delete $currentPath/$fileName"
+    # 全部加上双引号是因为文件名中有可能会有空格
     mv "$currentPath/$fileName" "$trashPath/$fileName.$deleteTime"
-    (lazyDelete &)
+}
+moveAll(){
+    list=`ls $1`
+    for file in $list; do
+        # echo "$file"
+        moveFile "$file"
+    done
 }
 log(){
     content=$1;
@@ -34,41 +69,19 @@ help(){
     printf "Run ./trash.sh $start <params> $end\n"
     printf "  $start%-16s$end%-20s\n" "-h|h|help" "show help"
     printf "  $start%-16s$end%-20s\n" "file/dir" "move file/dir to trash dir"
+    printf "  $start%-16s$end%-20s\n" "a \"pattern\"" "all pattern file "
 }
-# 延迟删除, 并隐藏屏蔽了信号, 不阻塞当前终端
-lazyDelete(){
-    result=`ps ux | grep trash.sh | wc -l`
-    # echo "$result"
-
-    # 限制只开一个进程, 为啥是3
-    if [ "$result" != "3" ];then
-        exit
-    else
-        fileNum=`ls $trashPath | wc -l`
-        while [ ! $fileNum = 0 ]; do
-            sleep $checkTime
-            log "check trash ..."
-            ls $trashPath | cat | while read line; do
-                currentTime=`date +%s`
-                removeTime=${line##*\.}
-                ((result=$currentTime-$removeTime))
-                # echo "$line | $result"
-                if [ $result -ge $timeOut ];then
-                    log "rm -rf $trashPath/$line"
-                    rm -rf "$trashPath/$line"
-                fi
-            done
-            fileNum=`ls $trashPath | wc -l`
-        done
-        log "  trash is empty. exit..."
-    fi
-}
+init
 case $1 in 
     -h | h | help)
         help
     ;;
+    a)
+        moveAll "$2"
+        (lazyDelete &)  
+    ;;
     *)
-        init
         moveFile "$1"
+        (lazyDelete &)
     ;;
 esac
