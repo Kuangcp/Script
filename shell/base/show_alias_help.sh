cc13081fcde373a8ec9a1ab4cd1f74fcb0894e99
kuangcp <kuangcp@aliyun.com>
2018-05-23 10:22:45 +0800

    完成shell版输出别名配置

--- shell/base/show_alias_help.sh
+++ shell/base/show_alias_help.sh
@@ -1,45 +1,74 @@
 #!/bin/dash
 
 # TODO 重写输出别名文件
-
-aliasFile="all_alias.conf"
-
+path=$(cd `dirname $0`; pwd)
+aliasFile=$path/all_alias.conf
+start='\033[0;32m'
+end='\033[0m'
 
 help(){
-    start='\033[0;32m'
-    end='\033[0m'
-    printf "运行：dash check_desktop.sh $start <params> $end\n"
+    printf "运行：dash show_alias_help.sh $start <params> $end\n"
     printf "  $start%-16s$end%-20s\n" "-h|h|help" "帮助"
-}
+    printf "  $start%-16s$end%-20s\n" "1234.." "指定展示文件输出"
+    printf "  $start%-16s$end%-20s\n" "-e 1234.." "详细输出"
+    printf "  $start%-16s$end%-20s\n" "-l" "输出配置文件"
+    printf "  $start%-16s$end%-20s\n" "-e" "编辑配置文件"
 
+}
+show(){
+    if [ $1"z" = "z" ];then
+        help
+        exit
+    fi
+    if [ "$1" -gt 0 ] 2>/dev/null ;then 
+        printf ""
+    else 
+        echo '请输入整数' 
+        exit
+    fi 
+    allLine=`sed -n '$=' $aliasFile`
+    if [ $1 -gt $allLine ]; then
+        echo "超出范围"
+        exit
+    fi
+    file=`sed -n ${1}p $aliasFile`
+    # echo $file
+    cat $file | while read line; do
+        title=`expr match "$line" "^#\s"`
+        if [ $title != 0 ]; then 
+            line=${line##*#}
+            printf "\n\033[0;35m[ %s ]$end\n" "$line"
+        fi
+        oneAlias=`expr match "$line" "^alias"`
+        if [ $oneAlias != 0 ];then
+            # echo $line
+            name=${line%=\'*}
+            name=${name##*alias}
+            command=${line#*=\'}
+            command=${command%\'*}
+            comment=${line##*\'}
+            if [ $2'z' = 'z' ];then 
+                printf "$start%-20s$end%-30s\n" "$name" "$comment"
+            else
+                printf "$start%-20s$end%-30s$start%s$end\n" "$name" "$comment" "$command"
+            fi
+        fi
+    done
+}
 case $1 in 
     -h | h | help)
         help
     ;;
+    -l)
+        less $aliasFile
+    ;;
+    -e)
+        vi $aliasFile
+    ;;
     -a)
-        cat $aliasFile
+        show $2 1
     ;;
     *)
-        if [ $1"z" = "z" ];then
-            echo "请指定别名文件"
-            exit
-        fi
-        allLine=`sed -n '$=' $aliasFile`
-        if [ $1 -gt $allLine ]; then
-            echo "超出范围"
-            exit
-        fi
-        file=`sed -n ${1}p $aliasFile`
-        echo $file
-        cat $file | while read line; do
-            # echo $line
-            clean=`expr match "$line" "^alias"`
-            if [ $clean != 0 ];then
-                echo $line
-                name=${line%=*}
-                name=${name##*alias}
-                echo $name
-            fi
-        done  
+        show $1
     ;;
 esac
