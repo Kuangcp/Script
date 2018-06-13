a210392cef67de7b3504a9649f53078d3b2c723d
kuangcp <kuangcp@aliyun.com>
2018-06-13 20:56:42 +0800

    记录时间序列

--- python/tool/key/Analysis.py
+++ python/tool/key/Analysis.py
@@ -1,13 +1,32 @@
 import datetime
 import fire
+import time
 from RecordClickWithRedis import get_conf
 from RecordClickWithRedis import get_conn
 
 file = '/main.conf'
-
-def count_num(date):
+def show_timeline(days=None):
+    date = caculate_day(days)
     global file
     conn = get_conn(file)
+    result = conn.zrange('detail-'+date, 0, -1, withscores=True)
+    if result is None or result == []:
+        print('没有记录')
+        return 
+    else:
+        all = get_key_map()
+        for key,value in result:
+            timestamp = key.decode()
+            timestamp = time.localtime(float(timestamp))
+            day = time.strftime('%Y-%m-%d_%H:%M:%S', timestamp)
+            name = all.get(str(int(value)).encode()).decode()
+            # TODO 结果是按按键来排序的, 如果按时间排序呢
+            print(day, name)
+
+def count_num(days=None):
+    global file
+    conn = get_conn(file)
+    date = caculate_day(days)
     total_num = conn.get('all-'+date)
     if total_num is None:
         print(date, '没有记录')
@@ -23,11 +42,15 @@ def count_num(date):
             name = name.decode()
         print("%-6s -- \033[0;32m%s\033[0m"%(key[1], name))
 
-def list_map():
+def get_key_map():
     global file
     conn = get_conn(file)
-    keys = []
     all = conn.hgetall('key_map')
+    return all
+
+def list_map():
+    keys = []
+    all = get_key_map()
     for key in all.keys():
         keys.append(int(key.decode()))
     keys.sort()
@@ -35,33 +58,41 @@ def list_map():
         value = all.get(str(key).encode())
         print(key,'=', value.decode())
 
-def show_day(days=None):
+def caculate_day(days=None):
+    ''' 计算当天日期, 和前推日期''' 
     if days is None:
-        today = datetime.datetime.now().strftime('%Y-%m-%d')
-        count_num(today)
+        result = datetime.datetime.now().strftime('%Y-%m-%d')
     else:
         now_time = datetime.datetime.now()
-        yes_time = now_time + datetime.timedelta(days)
-        new_time = yes_time.strftime('%Y-%m-%d')
-        count_num(new_time)
+        yes_time = now_time + datetime.timedelta(days*-1)
+        result = yes_time.strftime('%Y-%m-%d')
+    return result
 
 def show_help():
     start='\033[0;32m'
     end='\033[0m'
     print("%-26s %-20s"%(start+"-h"+end, "帮助"))
-    # print("%-26s %-20s"%(start+"-b num"+end, "展示 num 天前的记录"))
-    print("%-26s %-20s"%(start+"num"+end, "展示 num 天前的记录"))
+    print("%-26s %-20s"%(start+"-d <num>"+end, "缺省为今天, 展示 num 天前的详细时间线"))
+    print("%-26s %-20s"%(start+"<num>"+end, "缺省为今天, 展示 num 天前的记录"))
 
-def main(action=None):
+def main(action=None, day=0):
     if action is None:
-        show_day()
+        count_num()
         return
     if action == '-h':
         show_help()
         return 
+    if action == '-d':
+        # print('详细', caculate_day(day))
+        show_timeline(day)
+        return 
     if type(action) == int:
-        show_day(action*-1)
+        count_num(action)
     else:
         print('请输入正确的参数')
 
-fire.Fire(main)
+try:
+    fire.Fire(main)
+except(BrokenPipeError, IOError):
+    # 是因为 print还没有输出完, 被less接管了, 输出流中断了??
+    pass
--- python/tool/key/anaiysis.sh
+++ python/tool/key/anaiysis.sh
@@ -4,8 +4,9 @@ if [ $1'z' = '-hz' ];then
     python3 $path/Analysis.py -h
     exit
 fi
-if [ $1'z' = '-bz' ] && [ $2'z' = 'z' ];then
-    echo '请输入第二个参数'
-    exit
-fi
+# if [ $1'z' = '-bz' ] && [ $2'z' = 'z' ];then
+#     echo '请输入第二个参数'
+#     exit
+# fi
 python3 $path/Analysis.py $1 $2 | less
+
