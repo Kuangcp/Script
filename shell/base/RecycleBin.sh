235566303a91936d8580944908dfbe118f602259
kuangcp <kuangcp@aliyun.com>
2018-06-21 17:57:35 +0800

    彩色日志

--- shell/base/RecycleBin.sh
+++ shell/base/RecycleBin.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
-# 回收站
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+cyan='\033[0;34m'
+pulper='\033[0;35m'
+blue='\033[0;36m'
+white='\033[0;37m'
+end='\033[0m'
+
 userDir=(`cd && pwd`)
 currentPath=`pwd`
 trashPath=$userDir'/.RecycleBin'
 # 日志
 logFile=$userDir'/.all.RecycleBin.log'
 
-liveTime=3600 # 存活时间 1小时
-checkTime='10m' # 轮询周期 10分钟
+# liveTime=3600 # 存活时间 1小时
+# checkTime='10m' # 轮询周期 10分钟
 
 # DEBUG
-# liveTime=5 # 存活时间 5s
-# checkTime='1s' # 轮询周期 1s
-
-error='\033[0;31m'
-start='\033[0;32m'
-end='\033[0m'
+liveTime=5 # 存活时间 5s
+checkTime='1s' # 轮询周期 1s
 
 init(){
     if [ ! -d $trashPath ];then
@@ -35,20 +40,20 @@ lazyDelete(){
         fileNum=`ls -A $trashPath | wc -l`
         while [ ! $fileNum = 0 ]; do
             sleep $checkTime
-            log "→ timing detection ▌ check trash ..."
+            log "→ timing detection  ▌ check trash ..."
             ls -A $trashPath | cat | while read line; do
                 currentTime=`date +%s`
                 removeTime=${line##*\.}
                 ((result=$currentTime-$removeTime))
                 # echo "$line | $result"
                 if [ $result -ge $liveTime ];then
-                    log "■ true detetoin ▌ rm -rf $trashPath/$line"
+                    log_warn "■ true detetoin     ▌ rm -rf $trashPath/$line"
                     rm -rf "$trashPath/$line"
                 fi
             done
             fileNum=`ls -A $trashPath | wc -l`
         done
-        log "▶▶▶ trash is empty ▌ script will exit ..."
+        log_error "▶ trash is empty    ▌ script will exit ..."
     fi
 }
 moveFile(){
@@ -56,9 +61,9 @@ moveFile(){
     # echo "input file : $fileName"
     deleteTime=`date +%s`
     readable=`date +%Y-%m-%d_%H:%M:%S`
-    log "◆ prepare to delete ▌ $currentPath/$fileName"
+    log_info "◆ prepare to delete ▌ $currentPath/$fileName"
     if [ ! -f "$currentPath/$fileName" ] && [ ! -d "$currentPath/$fileName" ] && [ ! -L "$currentPath/$fileName" ];then 
-        printf $error"file not exist \n"
+        printf $red"file not exist \n"
         exit
     fi
     # 全部加上双引号是因为文件名中有可能会有空格
@@ -76,7 +81,7 @@ moveAll(){
     list=`ls $1`
     num=${#list}
     if [ $num = 0 ];then
-        printf $error"no matches found $1 \n"
+        printf $red"no matches found $1 \n"
         exit
     fi
     for file in $list; do
@@ -86,31 +91,39 @@ moveAll(){
 }
 rollback(){
     if [ "$1"1 = "1" ];then
-        printf $error"please select a specific rollback file\n"
+        printf $red"please select a specific rollback file\n"
         exit 1
     fi
     if [ ! -f $trashPath/$1 ] && [ ! -d $trashPath/$1 ];then
-        printf $error"this file not exist \n"
+        printf $red"this file not exist \n"
         exit 1
     fi
     file=${1%\.*}
     file=${file%\.*}
     mv $trashPath/$1 $file
-    log "◀ rollback file ▌ $file"
-    printf $start"rollback [$file] complete \n"
+    log_info "◀ rollback file ▌ $file"
+    printf $green"rollback [$file] complete \n"
 }
 log(){
-    content=$1;
-    echo `date +%y-%m-%d\ %H:%M:%S`" $content">>$logFile
+    printf "`date +%y-%m-%d_%H:%M:%S` $1\n" >>$logFile
+}
+log_error(){
+    printf `date +%y-%m-%d_%H:%M:%S`"$red $1\n" >>$logFile
+}
+log_info(){
+    printf `date +%y-%m-%d_%H:%M:%S`"$green $1\n" >>$logFile
+}
+log_warn(){
+    printf `date +%y-%m-%d_%H:%M:%S`"$yellow $1\n" >>$logFile
 }
 help(){
-    printf "Run ./RecycleBin.sh $start <params> $end\n"
-    printf "  $start%-16s$end%-20s\n" "-h|help" "show help"
-    printf "  $start%-16s$end%-20s\n" "file/dir" "move file/dir to trash dir"
-    printf "  $start%-16s$end%-20s\n" "-a \"pattern\"" "all pattern file "
-    printf "  $start%-16s$end%-20s\n" "-l " "list file in trash "
-    printf "  $start%-16s$end%-20s\n" "-b file" "rollback file from trash "
-    printf "  $start%-16s$end%-20s\n" "-log" "show log"
+    printf "Run ./RecycleBin.sh $green <params> $end\n"
+    printf "  $green%-16s$end%-20s\n" "-h|help" "show help"
+    printf "  $green%-16s$end%-20s\n" "file/dir" "move file/dir to trash dir"
+    printf "  $green%-16s$end%-20s\n" "-a \"pattern\"" "all pattern file "
+    printf "  $green%-16s$end%-20s\n" "-l " "list file in trash "
+    printf "  $green%-16s$end%-20s\n" "-b file" "rollback file from trash "
+    printf "  $green%-16s$end%-20s\n" "-log" "show log"
 }
 init
 case $1 in 
@@ -130,9 +143,17 @@ case $1 in
     -b)
         rollback $2
     ;;
+    -down)
+        # TODO 杀掉驻留进程
+        result=`ps ux | grep Recycle`
+        # echo "$result"
+        for line in $result; do
+            echo $line
+        done
+    ;;
     *)
         if [ "$1"1 = "1" ];then
-            printf $error"pelease select specific file\n"$end 
+            printf $red"pelease select specific file\n"$end 
             help
             exit
         fi
