23c855af60a05f882c32a88564975b780292853d
kuangcp <kuangcp@aliyun.com>
2018-06-21 21:14:22 +0800

    完成后台进程的手动停止

--- shell/base/RecycleBin.sh
+++ shell/base/RecycleBin.sh
@@ -12,28 +12,27 @@ end='\033[0m'
 userDir=(`cd && pwd`)
 currentPath=`pwd`
 trashPath=$userDir'/.RecycleBin'
-# 日志
 logFile=$userDir'/.all.RecycleBin.log'
 
-# liveTime=3600 # 存活时间 1小时
-# checkTime='10m' # 轮询周期 10分钟
+liveTime=3600 # 存活时间 1小时
+checkTime='10m' # 轮询周期 10分钟
 
 # DEBUG
-liveTime=5 # 存活时间 5s
-checkTime='1s' # 轮询周期 1s
+# liveTime=5 # 存活时间 5s
+# checkTime='1s' # 轮询周期 1s
 
 init(){
     if [ ! -d $trashPath ];then
         mkdir -p $trashPath
         touch $logFile
     fi
-    printf "Path : \033[0;32m"$trashPath"\n\033[0m"
+    printf "TrashPath : \033[0;32m"$trashPath"\n\033[0m"
 }
 # 延迟删除, 并隐藏屏蔽了信号, 不阻塞当前终端
 lazyDelete(){
-    result=`ps ux | grep RecycleBin.sh | wc -l`
+    result=`ps -ef | grep RecycleBin.sh | wc -l`
     # echo "$result"
-    # 限制只开一个进程, 为啥是3
+    # 限制只开一个进程, 3 是因为第一次运行才是3
     if [ "$result" != "3" ];then
         exit
     else
@@ -58,7 +57,6 @@ lazyDelete(){
 }
 moveFile(){
     fileName="$1";
-    # echo "input file : $fileName"
     deleteTime=`date +%s`
     readable=`date +%Y-%m-%d_%H:%M:%S`
     log_info "◆ prepare to delete ▌ $currentPath/$fileName"
@@ -117,7 +115,7 @@ log_warn(){
     printf `date +%y-%m-%d_%H:%M:%S`"$yellow $1\n" >>$logFile
 }
 help(){
-    printf "Run ./RecycleBin.sh $green <params> $end\n"
+    printf "Run : ./RecycleBin.sh $green <params> $end\n"
     printf "  $green%-16s$end%-20s\n" "-h|help" "show help"
     printf "  $green%-16s$end%-20s\n" "file/dir" "move file/dir to trash dir"
     printf "  $green%-16s$end%-20s\n" "-a \"pattern\"" "all pattern file "
@@ -125,6 +123,8 @@ help(){
     printf "  $green%-16s$end%-20s\n" "-b file" "rollback file from trash "
     printf "  $green%-16s$end%-20s\n" "-log" "show log"
 }
+
+# 初始化脚本的环境
 init
 case $1 in 
     -h | help)
@@ -144,12 +144,13 @@ case $1 in
         rollback $2
     ;;
     -down)
-        # TODO 杀掉驻留进程
-        result=`ps ux | grep Recycle`
-        # echo "$result"
-        for line in $result; do
-            echo $line
-        done
+        id=`ps -ef | grep "RecycleBin.sh" | grep -v "grep" | grep -v "\-down" | awk '{print $2}'`
+        if [ $id"1" = "1" ];then
+            printf $red"not exist background running script\n"$end
+        else
+            log_warn "user killed  script ▌ $id"
+            kill -9 $id
+        fi
     ;;
     *)
         if [ "$1"1 = "1" ];then
