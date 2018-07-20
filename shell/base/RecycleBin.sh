992245a3135a3407c15a71e88b54eb72311f065e
kuangcp <kuangcp@aliyun.com>
2018-07-20 10:03:15 +0800

    optimize RecycleBin.sh

--- shell/base/RecycleBin.sh
+++ shell/base/RecycleBin.sh
@@ -13,36 +13,41 @@ userDir=(`cd && pwd`)
 realPath=$(cd `dirname $0`; pwd)
 currentPath=`pwd`
 
-mainPath=$userDir'/.config/kuangcp/RecycleBin'
-trashPath=$mainPath'/trash'
-logDir=$mainPath'/log'
-cnfDir=$mainPath'/conf'
+mainDir=$userDir'/.config/kuangcp/RecycleBin'
+trashDir=$mainDir'/trash'
+logDir=$mainDir'/log'
+cnfDir=$mainDir'/conf'
 
 logFile=$logDir'/RecycleBin.log'
 configFile=$cnfDir'/main.ini'
 
 # 默认配置
-liveTime=86400 # 单位:s 存活时间 3天 86400=1天,  
-checkTime='10m' # 轮询周期 1小时 依赖 sleep实现 单位为: d h m s 
-
+# liveTime=86400 # 单位:s 存活时间 3天 86400=1天,  
+# checkTime='10m' # 轮询周期 1小时 依赖 sleep实现 单位为: d h m s 
 
 # TODO  就差一个记录文件的原始目录的逻辑, 这样就能达到回收站的全部功能了
 # TODO 文件名最大长度是255, 注意测试边界条件
 # TODO -l 时 显示链接文件时 格式全混乱了
 
 init(){
-    if [ ! -d $trashPath ];then
-        mkdir -p $trashPath
+    if [ ! -d $trashDir ];then
+        mkdir -p $trashDir
     fi
     if [ ! -d $logDir ];then
         mkdir -p $logDir
     fi
-    touch $logFile
-    touch $configFile
+    if [ ! -d $cnfDir ];then
+        mkdir -p $cnfDir
+    fi
 
-    printf "TrashPath : \033[0;32m"$mainPath"\n\033[0m"
-    
-    # 配置文件覆盖默认配置
+    if [ ! -f $logFile ];then
+        touch $logFile
+    fi
+    if [ ! -f $configFile ];then
+        echo -e "liveTime=86400\ncheckTime='10m'" > $configFile
+    fi
+
+    printf "TrashPath : \033[0;32m"$mainDir"\n\033[0m"
     . $configFile
 }
 # 延迟删除, 并隐藏屏蔽了信号, 不阻塞当前终端
@@ -53,21 +58,21 @@ lazyDelete(){
     if [ "$result" != "3" ];then
         exit
     else
-        fileNum=`ls -A $trashPath | wc -l`
+        fileNum=`ls -A $trashDir | wc -l`
         while [ ! $fileNum = 0 ]; do
             sleep $checkTime
             logInfoWithWhite "→ timing detection  ▌ check trash ..."
-            ls -A $trashPath | cat | while read line; do
+            ls -A $trashDir | cat | while read line; do
                 currentTime=`date +%s`
                 removeTime=${line##*\.}
                 ((result=$currentTime-$removeTime))
                 # echo "$line | $result"
                 if [ $result -ge $liveTime ];then
-                    logWarn "▶ real delete       ▌ rm -rf $trashPath/$line"
-                    rm -rf "$trashPath/$line"
+                    logWarn "▶ real delete       ▌ rm -rf $trashDir/$line"
+                    rm -rf "$trashDir/$line"
                 fi
             done
-            fileNum=`ls -A $trashPath | wc -l`
+            fileNum=`ls -A $trashDir | wc -l`
         done
         logError "▶ trash is empty    ▌ script will exit ..."
     fi
@@ -84,7 +89,7 @@ moveFile(){
     fi
     logInfoWithGreen "◆ prepare to delete ▌ $currentPath/$fileName"
     # 全部加上双引号是因为文件名中有可能会有空格
-    mv "$currentPath/$fileName" "$trashPath/$fileName.$readable.$deleteTime"
+    mv "$currentPath/$fileName" "$trashDir/$fileName.$readable.$deleteTime"
 }
 # * 通配符删除
 moveAll(){
@@ -123,13 +128,13 @@ rollback(){
         printf $red"please select a specific rollback file\n"
         exit 1
     fi
-    if [ ! -f $trashPath/$1 ] && [ ! -d $trashPath/$1 ];then
+    if [ ! -f $trashDir/$1 ] && [ ! -d $trashDir/$1 ];then
         printf $red"this file not exist \n"
         exit 1
     fi
     file=${1%\.*}
     file=${file%\.*}
-    mv $trashPath/$1 $file
+    mv $trashDir/$1 $file
     logInfoWithCyan "◀ rollback file     ▌ $file"
     printf $green"rollback [$file] complete \n"
 }
@@ -153,14 +158,15 @@ logWarn(){
 
 help(){
     printf "Run：$red sh RecycleBin.sh$green <verb> $yellow<args>$end\n"
-    format="  $green%-4s $yellow%-15s$end%-20s\n"
+    format="  $green%-5s $yellow%-15s$end%-20s\n"
     printf "$format" "any" "" "move file/dir to trash"
     printf "$format" "-h" "" "show help"
     printf "$format" "-a" "\"pattern\"" "delete file (can't use *, actually command: 'ls | grep \"pattern\"')"
     printf "$format" "-l" "" "list all file in trash(exclude link file)"
-    printf "$format" "-b" "file" "rollback file from trash"
+    printf "$format" "-roll" "file" "rollback file from trash"
     printf "$format" "-lo" "file" "show log"
     printf "$format" "-cnf" "" "edit main config file "
+    printf "$format" "-b" "" "show background running script"
     printf "$format" "-d" "" "shutdown this script"
 }
 
@@ -175,7 +181,7 @@ showNameColorful(){
 }
 listTrashFiles(){
     # grep r 是为了将一行结果变成多行, 目前不展示link文件
-    file_list=`ls -lAFh $trashPath | egrep -v '^lr' | grep 'r'`
+    file_list=`ls -lAFh $trashDir | egrep -v '^lr' | grep 'r'`
     count=0
     printf "$blue%-9s %-3s %-5s %-5s %-5s%-5s %-5s %-5s %-19s %-5s$end\n" "mode" "num" "user" "group" "size" "month" "day" "time " "datetime" "filename "
     printf "${blue}---------------------------------------------------------------------------------------- $end\n"
@@ -198,6 +204,7 @@ upgrade(){
 
 # 初始化脚本的环境
 init
+
 case $1 in 
     -h)
         help
@@ -212,8 +219,11 @@ case $1 in
     -l)
         listTrashFiles
     ;;
-    -b)
+    -roll)
         rollback $2
+    ;;
+    -b)
+        ps aux | grep RSS | grep -v "grep" && ps aux | egrep -v "grep" | grep RecycleBin.sh | grep -v "RecycleBin.sh -b"
     ;;
     -d)
         id=`ps -ef | grep "RecycleBin.sh" | grep -v "grep" | grep -v "\-d" | awk '{print $2}'`
--- shell/deploy/deploy_boot_jar.sh
+++ shell/deploy/deploy_boot_jar.sh
@@ -2,4 +2,6 @@ rootDir='/home/rs/synthesizerd'
 
 dateTime=`date +%m_%d-%H_%M_%S`
 
-ssh rs@192.168.10.201 "sh $rootDir/bin/stop.sh && mv $rootDir/synthesizer.jar $rootDir/backup/synthesizer.jar_$dateTime && mv $rootDir/upload/synthesizer.jar $rootDir/synthesizer.jar && sh $rootDir/bin/start.sh" 
+# ./bin/stop.sh && mv synthesizer.jar backup/synthesizer.jar`date +%m_%d-%H_%M_%S` && mv upload/synthesizer.jar . && ./bin/start.sh
+# ssh rs@192.168.10.201 "cd "$rootDir" && sh bin/stop.sh && mv synthesizer.jar backup/synthesizer.jar_$dateTime && mv upload/synthesizer.jar . && sh bin/start.sh" 
+ssh rs@192.168.10.201 "cd "$rootDir" && ls"
