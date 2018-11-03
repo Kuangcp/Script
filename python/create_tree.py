09c8fccd3b714d63a54945184e156866875674fc
mythos <kuangcp@aliyun.com>
2018-11-03 13:28:49 +0800

    update alias config script

--- python/create_tree.py
+++ python/create_tree.py
@@ -2,122 +2,111 @@ import os
 import sys
 import getopt
 import subprocess
+import fire
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+end='\033[0m'
+
 
 '''
-    通过运行脚本，读取文件夹的文件生成目录，为了gitbook
-    使用： python3 create_tree -h 查看帮助
+    通过运行脚本，读取文件夹的文件生成目录，为了gitbook和wiki
+    使用： python3 create_tree.py -h 查看帮助
 '''
 # 忽略的文件夹
-dirs=['.git','SQL','Docker']
+ignoreFolder=['.git', 'backup', '.vscode']
 # 所有要被忽略的文件
-files=['PULL_REQUEST_TEMPLATE.md', 'ISSUE_TEMPLATE.md', 'CODE_OF_CONDUCT.md', 'README.md', 'CSS3.md', 'HTML5.md']
-flag_show=False
-flag_append=False
+ignoreFile=['PULL_REQUEST_TEMPLATE.md', 'ISSUE_TEMPLATE.md', 'CODE_OF_CONDUCT.md','README.md', 
+    'Readme.md', 'CSS3.md', 'HTML5.md', '_Sidebar.md']
+
 result = []
 
-def show_help():
-    print("使用: python3 create_tree.py [-s] [-a] [-h] ")
-    print("参数说明: \n -s     : 控制台显示生成的目录\n -a     : 生成的目录追加到文件中去\n -h     : 帮助")
+def logError(msg):
+    print('%s%s%s'%(red, msg, end))
 
-# 列出所有文件的列表
-def listfiles(name):
-	#print('_________',name,not os.path.isdir(name))
+def logInfo(msg):
+    print('%s%s%s'%(green, msg, end))
+
+def printParam(verb, args, comment):
+    print('  %s%-5s %s%-6s %s%s'%(green, verb, yellow, args, end, comment))
+
+def help():
+    print('run: %s  %s <verb> %s <args>%s'%('create_tree.py', green, yellow, end))
+    printParam('-h', '', 'help')
+    printParam('-s', '', 'show catalog')
+    printParam('-a', '', 'append cataloag to SUMMARAY.md')
+
+# 列出所有文件的列表 并排好序
+def listFiles(name):
     lists = []
     dir_list = []
     temp = os.listdir(name)
     temp.sort()
-    # print(temp)
     for r in temp:
         if(not os.path.isdir(name+'/'+r)):
             lists.append(r)
         else:
-            #print("是目录:::::::::::",r)
             dir_list.append(r)
-            #lists.insert(0,r)
     lists.sort()
     lists = dir_list + lists
     return lists 
         
-# 输出行
-def print_line(name,count,path):
-    temp=''
-    for i in range(1,count+1,1):
-        temp = temp+'    '
-    temp = temp+'* '
-    # print(temp,'[-',name,'-](./',path,')')
-    if not name in files:
-        result.append(temp+'[ '+name[:-3]+' ](./'+path+')')
+# 处理文件
+def handlerFile(name, count, path):
+    temp = '    '*count
+    if not name in ignoreFile:
+        result.append(temp+'* [ '+name[:-3]+' ](/'+path+')')
 
+# 处理标题(文件夹)
+def handlerFolder(name, count):
+    temp = '    '*(int(count)-1)
+    result.append(temp+'* 【 '+name+' 】') 
 
-def print_title(name,count):
-    ''''''
-    temp=''
-    for i in range(1,count,1):
-        temp = temp+'    '
-    temp = temp+'* '
-    # 输出文件夹目录
-    #print(temp,name.split('/'))
-    result.append(temp+'【 '+name+' 】') 
+# 递归 读取文件夹
+def readFolder(name, count):
+    handlerFolder(name,count)
+    for fold in listFiles(name):
+        if fold in ignoreFolder:
+            continue
+        if not os.path.isdir(name+'/'+fold):
+            handlerFile(fold, count, name+'/'+fold)
+        else:
+            readFolder(name+'/'+fold, count+1)
 
+def readAll():
+    Folders = os.listdir('./')
+    Folders.sort()
+    # 处理根目录下的md文件
+    for fold in Folders:
+        if fold.endswith('.md') and not fold in ignoreFile:
+            result.append("* [ "+fold[:-3]+" ](./"+fold+")")
+    # 得到根目录下所有文件夹，然后开始递归得到所有文件       
+    for fold in Folders:
+        if os.path.isdir(fold) and not fold in ignoreFolder :
+            readFolder(fold, 1)
 
-# 递归
-def create(name,count):
-    if(os.path.isdir(name)):
-        print_title(name,count)
-        for fold in listfiles(name):
-            if fold in dirs:
-                continue
-            if not os.path.isdir(name+'/'+fold):
-                #print("不是目录--------",fold)
-                print_line(fold,count,name+'/'+fold)
-            
-            create(name+'/'+fold,count+1)
-    else:
+def main(verb=None):
+    if verb == '-h':
+        help()
         return
-
-# 处理参数
-opts, args = getopt.getopt(sys.argv[1:], "sha")
-for op,value in opts:
-    if op == "-s":
-        flag_show = True
-        #print("show")
-    elif op == "-a":
-        flag_append = True
-        #print("append")
-    elif op == "-h":
-        show_help()
-
-
-Folders = os.listdir('./')
-Folders.sort()
-
-# 处理根目录下的md文件
-for fold in Folders:
-    if fold.endswith('.md'):
-        if fold in files:
-            continue
-        #print("md::::"+fold)
-        result.append("* [ "+fold[:-3]+" ](./"+fold+")")
-
-# 得到根目录下所有文件夹，然后开始递归得到所有文件       
-for fold in Folders:
-    if(os.path.isdir(fold)):
-        if(fold in dirs):
-            continue
-        create(fold,1)
-
-
-# 终端输出
-if flag_show : 
-    for res in result:
-        print(res)     
-
-# 追加到gitbook的目录文件中
-if flag_append :
-    subprocess.call('mv SUMMARY.md SUMMARY.md.bak',shell=True)
-    with open('SUMMARY.md','w+') as dest:
-        dest.write('# Summary\n\n* [Introduction](README.md)\n\n')
+    # 只在终端输出
+    if verb == "-s":
+        readAll()
         for res in result:
-            dest.write(res+'\n')
-    print('重新生成目录树完成!')
-        
+            print(res)
+    # 追加到SUMMARY
+    if verb == "-a":
+        readAll()
+        subprocess.call('mv SUMMARY.md SUMMARY.md.bak',shell=True)
+        with open('SUMMARY.md','w+') as dest:
+            dest.write('# Summary\n\n* [ Introduction ](README.md)\n\n')
+            for res in result:
+                dest.write(res+'\n')
+        logInfo('重新生成目录树完成!')
+
+fire.Fire(main)
--- shell/base/show_alias_help.sh
+++ shell/base/show_alias_help.sh
@@ -5,6 +5,12 @@ aliasFile=$path/all_alias.conf
 start='\033[0;32m'
 end='\033[0m'
 
+init(){
+    if [ ! -f $aliasFile ];then
+        printf ".customized.ini\n.path.ini\n.repos.ini\n.system.ini" >> $aliasFile
+    fi
+}
+
 help(){
     printf "运行：dash show_alias_help.sh $start <params> $end\n"
     printf "  $start%-16s$end%-20s\n" "-h|h|help" "帮助"
@@ -12,7 +18,6 @@ help(){
     printf "  $start%-16s$end%-20s\n" "-a 1234.." "详细输出"
     printf "  $start%-16s$end%-20s\n" "-l" "输出配置文件"
     printf "  $start%-16s$end%-20s\n" "-e" "编辑配置文件"
-
 }
 show(){
     if [ $1"z" = "z" ];then
@@ -31,8 +36,7 @@ show(){
         exit
     fi
     file=`sed -n ${1}p $aliasFile`
-    # echo $file
-    cat $file | while read line; do
+    cat ~/$file | while read line; do
         title=`expr match "$line" "^#\s"`
         if [ $title != 0 ]; then 
             line=${line##*#}
@@ -54,24 +58,17 @@ show(){
         fi
     done
 }
+
+init
 case $1 in 
-    -h | h | help)
-        help
-    ;;
-    -l)
-        less $aliasFile
-    ;;
-    -e)
-        vi $aliasFile
-    ;;
+    -h | h | help) help ;;
+    -l) less $aliasFile ;;
+    -e) vi $aliasFile ;;
     -a)
         if [ $2'z' = 'z' ];then
             echo "请指定文件索引"
             exit
         fi
-        show $2 1 | less
-    ;;
-    *)
-        show $1 | less
-    ;;
+        show $2 1 | less;;
+    *) show $1 | less ;;
 esac
