9da3f554d3e6a58283a652fe552feb1d2b08327d
kuangcp <kuangcp@aliyun.com>
2018-11-21 16:37:03 +0800

    optimized performance script

--- shell/base/Performance.sh
+++ shell/base/Performance.sh
@@ -11,13 +11,24 @@ cyan='\033[0;36m'
 white='\033[0;37m'
 end='\033[0m'
 
+userDir=(`cd && pwd`)
+logDir=$userDir'/.config/app-conf/log'
+
+init(){
+    if [ ! -d $logDir ];then
+        mkdir -p $logDir
+    fi
+}
+
 help(){
     printf "Run：$red bash Performance.sh $green<verb> $yellow<args>$end\n"
-    format="  $green%-6s $yellow%-8s$end%-20s\n"
+    format="  $green%-8s $yellow%-20s$end%-20s\n"
     printf "$format" "-h" "" "帮助"
-    printf "$format" "-s|s" "" "进程状态 按内存降序"
-    printf "$format" "-p|p" "" "按名称查看相关进程"
-    printf "$format" "-pm|pm" "" "按名称查看相关进程的使用内存统计"
+    printf "$format" "" "[processName]" "所有或指定进程状态 按内存降序"
+    printf "$format" "-p|p" "process interval" "按名称查看相关进程 或者按时间间隔一直查看进程信息"
+    printf "$format" "-pm|pm" "processName" "按名称查看相关进程的使用内存统计"
+    printf "$format" "-ss|ss" "[count]" "查看内存占用最多的几个进程 count默认40"
+    printf "$format" "watch" "processName" "10s统计进程总内存 输出到 $logDir"
 }
 
 showProcessByName(){
@@ -33,15 +44,26 @@ showAllProcess(){
     ps aux | grep -v RSS | awk '{print $6 "\t'$yellow'" $6/1024 "'$end'\t" $2 "\t'$green'" $11 "'$end'"}' | sort --human-numeric-sort -r
 }
 
+statisticsMemory(){
+    ps aux | egrep -v "grep" | grep -i $1 | awk '{sum+=$6};END {sum-=2800;print sum "K " sum/1024"M "}'
+}
+
+watchProcess(){
+    date +%s | xargs printf
+    printf "$green `date "+%Y-%m-%d %H:%M:%S"` $end"
+    statisticsMemory $1 
+    sleep 10
+}
+
+init 
+
 case $1 in 
     -h|h)
         help ;;
-    -s | s)
-        showAllProcess | less
-    ;;
     -p | p)
 		if [ "$3"z = "z" ];then
-            showProcessByName $2 
+            echo 'the second param invalid'
+            exit 1
 		else
 			while true; do
 				showProcessByName $2 
@@ -51,7 +73,7 @@ case $1 in
 		fi
     ;;
     -pm | pm)
-        ps aux | egrep -v "grep" | grep -i $2 | awk '{sum+=$6};END {sum-=2800;print sum "K " sum/1024"M "}'
+        statisticsMemory $2
     ;;
     -ss | ss)
         displayCount=40
@@ -71,6 +93,11 @@ case $1 in
             showAllProcess | head -n $displayCount
             sleep 3
             clear
+        done
+    ;;
+    -watch | watch)
+        while true; do
+            watchProcess $2 >> $logDir/$2.process.log
         done
     ;;
     *)
