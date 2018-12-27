d3e82013a7ef1e38eed7e23e686dc708645503fd
kuangcp <kuangcp@aliyun.com>
2018-12-27 14:31:22 +0800

    distinguish search directory and file

--- shell/base/FileTool.sh
+++ shell/base/FileTool.sh
@@ -17,8 +17,18 @@ help(){
     printf "$format" "-h" "" "帮助"
     printf "$format" "" "" "复制当前路径到粘贴板"
     printf "$format" "-f|f" "filename" "当前路径递归搜索文件"
+    printf "$format" "-d|d" "dirname" "当前路径递归搜索目录"
     printf "$format" "-p|p" "relative path" "输出相对路径的绝对路径并复制到粘贴板"
     printf "$format" "-cf|cf" "relative path" "复制文件内容到粘贴板"
+}
+
+checkParamCount(){
+    actual=$1
+    expect=$2
+    if [ ! $1 = $2 ]; then
+        printf "$red please specific fileName $end \n"
+        exit 1
+    fi
 }
 
 case $1 in 
@@ -32,15 +42,17 @@ case $1 in
 	-cf | cf)
 		cat $2 | xclip -sel clip
 	;;
+    -d | d)
+        checkParamCount $# 2
+        find . -type d -iname "*$2*" 
+    ;;
     -f | f)
-        if [ "$2"z = "z" ];then
-            echo "please specific fileName"
-            exit 1
-        fi
-        find . -iname "*$2*" 
+        checkParamCount $# 2
+        find . -type f -iname "*$2*" 
     ;;
     *)
         currentPath=`pwd`
         printf $currentPath/$1 | xclip -sel clip
     ;;
 esac
+
