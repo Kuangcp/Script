5984bf156f2ba9c82f3eb5ef81f457171fa05681
kuangcp <kuangcp@aliyun.com>
2019-01-02 11:17:34 +0800

    optimized logic

--- shell/assistant/python_unittest.sh
+++ shell/assistant/python_unittest.sh
@@ -16,8 +16,6 @@ help(){
     printf "$format" "filename" "[testcase]" "show all testcase when testcase is empty otherwise run test case"
 }
 
-# TODO 实现的功能 读取 Python 单元测试文件, 识别出其中的测试类, 测试方法, 然后交互式使用户选择运行哪个方法, 类
-
 log(){
     printf " $1\n"
 }
@@ -32,7 +30,7 @@ log_warn(){
 }
 read_content(){
     filename=$1
-    class_name=''
+    log_info "all testcase: "
     cat $filename | while read line; do
         class_define=$(echo "$line" | grep -E "^class\s.*\(.*TestCase\):$")
         if [ ! "$class_define"z = 'z' ]; then
@@ -40,15 +38,19 @@ read_content(){
             class_name=${temp%%(*}
             printf "\n%s\n" $class_name
         fi
-        # echo "$line"
-        method_define=$(echo "$line" | grep -E "^.*def\stest.*\(self\):$")
-        # echo $method_define
-        if [ ! "$method_define"z = 'z' ];then
-            temp=${line#*def}
-            method_name=${temp%%(*}
-            printf "%s.%s\n" $class_name $method_name
-        fi
+        if [ ! "$class_name"z = "z" ]; then
+            method_define=$(echo "$line" | grep -E "^.*def\stest.*\(self\):$")
+            # echo $method_define
+            if [ ! "$method_define"z = 'z' ];then
+                temp=${line#*def}
+                method_name=${temp%%(*}
+                printf "    %s.%s\n" $class_name $method_name
+            fi
+       fi
     done
+   #  if [ $count = 0 ]; then
+   #     log_error "not exist any testcase in $filename"
+   # fi
 }
 case $1 in 
     -h)
