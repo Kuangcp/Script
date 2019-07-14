ee8b69abcb52d745111c46eb6ba4913d8356352d
Kuangcp <kuangcp@aliyun.com>
2019-07-14 10:41:04 +0800

    +) down m3u8

---
+++ shell/media/mergets.sh
@@ -0,0 +1,29 @@
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+end='\033[0m'
+
+help(){
+    printf "Run：$red sh mergets.sh $green<verb> $yellow<args>$end\n"
+    format="  $green%-6s $yellow%-8s$end%-20s\n"
+    printf "$format" "-h" "" "help"
+    printf "$format" "" "url" "url of m3u8, start download"
+}
+
+case $1 in
+    -h)
+        help ;;
+    *)
+        file=$(date +%F_%T).mp4
+        ffmpeg -i $1 -c copy -bsf:a aac_adtstoasc $file
+        echo "finished $file"
+    ;;
+esac
+
+
+
