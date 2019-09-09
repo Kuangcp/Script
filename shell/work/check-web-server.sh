9b1834e0a99b22528b68b4b9a82a76422b798cf4
kcp <kuangcp@aliyun.com>
2019-09-09 17:55:05 +0800

    watch server status

--- shell/work/check-web-server.sh
+++ shell/work/check-web-server.sh
@@ -9,18 +9,19 @@ end='\033[0m'
 
 userDir=(`cd && pwd`)
 cache_page="$userDir/.config/app-conf/log/healthcheck_status"
+icon_file='/home/kcp/Application/Icon/Stream.svg'
 
 log(){
     printf " $1\n"
 }
 log_error(){
-    printf "$red $1 $end\n" 
+    printf `date +%y-%m-%d_%H:%M:%S`"$red $1 $end\n" 
 }
 log_info(){
-    printf "$green $1 $end\n" 
+    printf `date +%y-%m-%d_%H:%M:%S`"$green $1 $end\n" 
 }
 log_warn(){
-    printf "$yellow $1 $end\n" 
+    printf `date +%y-%m-%d_%H:%M:%S`"$yellow $1 $end\n" 
 }
 
 help(){
@@ -35,27 +36,21 @@ help(){
 update_cache(){
     rm -f $cache_page
     curl http://test.status.qipeipu.net/healthcheck_status -o $cache_page > /dev/null 2>&1
-    log_info "\n    update: "$cache_page
+    log_info "   update: "$cache_page
 }
 
 
 find_ip_by_app(){
     params="$*"
     regex=${params// /.*}
-    # echo $regex
-    result=$(cat $cache_page | grep "$regex" -A 2)
-    count=$(echo $result | wc -w)
-    if test $count != 3; then
-        log_error "more than one matched"
-        echo "$result" | less
-        exit 0
-    fi
+    result=$(find_server $*)
+
     for line in $result; do
         # log_info $line
         is_title=$(echo $line | grep "$regex")
         # echo "title:["$is_title"]"
         if test -n "$is_title"; then
-            printf "\n    "$(remove_td_tag $line)
+            printf "==>"$(remove_td_tag $line)
         fi
         if [[  $line == *'8080'* ]];then
             url=$(remove_td_tag $line)
@@ -68,29 +63,85 @@ find_ip_by_app(){
     done
 }
 
+find_server(){
+    if test $# == 0 ;then
+        log_error "please input any param \n"
+        help
+        exit 0
+    fi 
+    params="$*"
+    regex=${params// /.*}
+    # echo $regex
+    result=$(cat $cache_page | grep "$regex" -A 2)
+    count=$(echo $result | wc -w)
+    if test $count != 3; then
+        log_error "more than one matched"
+        echo "$result" | less
+        exit 0
+    fi
+    echo "$result"
+}
+
 remove_td_tag(){
     str=${1/<td>/}
     str=${str/<\/td>/}
     echo $str
 }
 
+watch_server_up(){
+    update_cache
+    params="$*"
+    regex=${params// /.*}
+    result=$(find_server $*)
+
+    down_flag=0
+    project_name=''
+    for line in $result; do
+        # log_info $line
+        is_title=$(echo $line | grep "$regex")
+        # echo "title:["$is_title"]"
+        if test -n "$is_title"; then
+            project_name=$(remove_td_tag $line)
+            printf "==> "$project_name
+        fi
+        if [[  $line == *'8080'* ]];then
+            url=$(remove_td_tag $line)
+            echo "  "$url
+        fi
+        if [[ $line == *"down"* ]]; then
+            down_flag=1
+            sleep 3;
+            watch_server_up $*
+        fi
+        if [[ $line == *"up"* ]] && [ $down_flag = 1 ]; then
+            msg="$project_name has already up"
+            notify-send -i $icon_file "$msg" -t 4000
+            log_info "$msg"
+            exit 0
+        fi
+        if [[ $line == *"up"* ]] && [ $down_flag = 0 ]; then
+            sleep 5;
+            watch_server_up $*
+        fi
+    done
+}
+
 case $1 in 
     -up|up)
         update_cache
     ;;
+    -w|w|watch)
+        watch_server_up ${*:2}
+    ;;
     -cnf|cnf)
+        update_cache
         less $cache_page
     ;;
     -h|h)
         help 
     ;;
     *)
-        if test $# == 0 ;then
-            log_error "please input any param \n"
-            help
-            exit 0
-        fi 
-        update_cache
+        # update_cache
         find_ip_by_app $*
     ;;
 esac
