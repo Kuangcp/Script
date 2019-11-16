fd8a5d14a7d668742fd34c2e3e4ec3869307853d
Kuangcp <kuangcp@aliyun.com>
2019-11-16 14:34:59 +0800

    *) copy from manjaro

--- shell/base/file-tool.sh
+++ shell/base/file-tool.sh
@@ -96,29 +96,25 @@ add_python_sys_path(){
     log_info "add success: $lib_path/$version/dist-packages/$filename.pth"
 }
 
-decompress_file(){
-    file=$1
-    suffix=${file##*\.}
-    # log_info "start extract: "$suffix
-    case $suffix in
-        tgz)
-            tar xzf $file
-        ;;
-        gz)
-            has_tar=$(echo $file| grep '.tar')
-            if test -z $has_tar;then
-                gzip -d $file
-            else
-                tar xzf $file
-            fi
-        ;;
-        zip)
-            unzip $file
-        ;;
-        *)
-            log_warn "not support this type"
-        ;;
+decompress_file (){
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1     ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *)           echo "'$1' cannot be extracted via ex()" ;;
     esac
+  else
+    echo "'$1' is not a valid file"
+  fi
 }
 
 case $1 in 
