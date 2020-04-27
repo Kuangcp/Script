05beea0a39fb078286d301b6726562e0155e4aeb
kcp <kuangcp@aliyun.com>
2020-04-28 01:01:47 +0800

    +) daily count

--- shell/base/git-tool.sh
+++ shell/base/git-tool.sh
@@ -21,6 +21,25 @@ case $1 in
         fi
         diff_branch $2 $3
     ;;
+    -s)
+        if test -z "$2"; then 
+            exit 1;
+        fi
+        au=''
+        if test -n "$3"; then 
+            au="--author=$3"
+        fi
+        echo $au
+        for i in $(seq 0 $2); do 
+            startDate=$(date -d "$(($i+1)) day ago" +"%Y-%m-%d")
+
+            data=$(git log --oneline --decorate --stat $au --since="$(($i+1)) day ago" --until="$i day ago" | grep "files changed")
+            add=$(echo "$data" | awk '{print $4}' | awk '{sum += $1};END {print sum}')
+            del=$(echo "$data" | awk '{print $6}' | awk '{sum += $1};END {print sum}')
+
+            printf "$cyan$startDate$end add:$green%-6s$end del:$red%-6s$end\n" $add $del 
+        done 
+    ;;
     -h)
         help ;;
     *)
