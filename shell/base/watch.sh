97f742a43969cea04c13bc807f316cd2c9cfc759
Kuangcp <kuangcp@aliyun.com>
2023-12-01 01:53:44 +0800

    watch

---
+++ shell/base/watch.sh
@@ -0,0 +1,27 @@
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+end='\033[0m'
+
+help(){
+    printf "Run：$red sh $0 $green<verb> $yellow<args>$end\n"
+    format="  $green%-6s $yellow%-8s$end%-20s\n"
+    printf "$format" "-h" "" "帮助"
+}
+
+watch(){
+    cmd=$1
+    source ~/.zshrc
+    $cmd
+}
+
+case $1 in 
+    -h)
+        help ;;
+    *)
+        watch $*;;
+esac
