964af4264c547397b174e8ce61b3052e9e545720
kcp <kuangcp@aliyun.com>
2024-09-27 18:53:27 +0800

    pg

--- database/postgresql/ddl.sql
+++ database/postgresql/ddl.sql
@@ -29,6 +29,29 @@ GROUP BY
     nspname,
     relname;
 
+---
+-- 查询表的 owner
+SELECT c.relname AS table_name, r.rolname AS table_owner
+FROM pg_class c
+         INNER JOIN pg_roles r ON c.relowner = r.oid
+         INNER JOIN pg_namespace n ON c.relnamespace = n.oid
+WHERE c.relkind = 'r'
+  AND n.nspname = 'public'
+  AND c.relname = 't_user';
+
+--- 
+
+-- 查询表占用的存储
+SELECT
+    pg_relation_size(pg_class.oid) AS table_size,
+    pg_class.relname AS table_name
+FROM
+    pg_class
+        LEFT JOIN pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+WHERE
+        pg_namespace.nspname = 'public'
+  AND pg_class.relname = 't_user';
+
 
 ---
 
@@ -41,3 +64,34 @@ WHERE c.relkind = 'r'
 and c.relname not like '%_1_prt_%' -- 排除分区表
  AND c.relname in (select table_name from INFORMATION_SCHEMA.role_table_grants where grantee = 'who' and  privilege_type='SELECT') -- 过滤who用户下有select权限的表
  ORDER BY c.relname;
+
+--- 
+
+-- 查询表上关联的索引
+
+select i.indrelid::regclass table_name,c.oid::regclass index_name,a.amname index_type
+       ,pg_catalog.pg_get_indexdef(i.indexrelid, 0, true)
+from pg_class c
+join pg_index i on c.oid=i.indexrelid
+join pg_am a on c.relam= a.oid
+where i.indrelid='public.t_user'::regclass;
+
+---
+
+-- 查询表的分区信息
+
+WITH att_arr AS (SELECT unnest(paratts) 
+        FROM pg_catalog.pg_partition p 
+        WHERE p.parrelid = (SELECT c.oid
+FROM pg_catalog.pg_class c
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE n.nspname||'.'||c.relname = 'public.t_user')  AND p.paristemplate = false
+        ), 
+idx_att AS (SELECT row_number() OVER() AS idx, unnest AS att_num FROM att_arr) 
+SELECT attname FROM pg_catalog.pg_attribute, idx_att 
+        WHERE attrelid=(SELECT c.oid
+FROM pg_catalog.pg_class c
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE n.nspname||'.'||c.relname = 'public.t_user') AND attnum = att_num ORDER BY idx
+
+
